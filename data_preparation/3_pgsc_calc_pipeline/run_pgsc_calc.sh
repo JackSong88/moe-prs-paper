04826d981e9ec748c885e20554e32af81c9af0c5
Shadi <shadizabad1@gmail.com>
2026-02-13 15:30:35 +0000

    Bug fixes

--- data_preparation/3_pgsc_calc_pipeline/run_pgsc_calc.sh
+++ data_preparation/3_pgsc_calc_pipeline/run_pgsc_calc.sh
@@ -16,8 +16,8 @@ module load apptainer
 # Options set by user:
 biobank=${1:-"cartagene"}
 target_build=${2:-"GRCh38"}
-scorefiles=${3:-"data/pgs_weights/*/${GRCh38}/*.txt.gz"}
-output_dir=${4:-"data/pgsc_calc_scores/$biobank/"}
+scorefiles=${3:-"data/pgs_weights/*/${target_build}/*.txt.gz"}
+output_dir=${4:-"data/pgsc_calc_scores/${biobank}/"}
 min_overlap=${5:-0.75}
 
 # Set nextflow environment variables:
@@ -31,30 +31,17 @@ export NXF_WORK=$(pwd)/nextflow_home/work/"$biobank"
 mkdir -p "$NXF_WORK"
 mkdir -p "$output_dir"
 
-# Parse optional parameters:
-extra_params=()
-
-# If the estimator is windowed, add the window size to the parameters:
-# else, add the path to the LDetect blocks for the block estimator:
-if [[ "${biobank}" == "cartagene" ]]
-then
-  extra_params+=("--liftover")
-  extra_params+=("--hg19_chain" "pgsc_calc_requirements/liftover_chains/hg19ToHg38.over.chain.gz")
-  extra_params+=("--hg38_chain" "pgsc_calc_requirements/liftover_chains/hg38ToHg19.over.chain.gz")
-fi
-
 # Run the pipeline:
 "$NXF_HOME"/nextflow run pgscatalog/pgsc_calc \
             -profile singularity \
             -offline \
             -c data_preparation/3_pgsc_calc_pipeline/custom.config \
             --input "pgsc_calc_requirements/samplesheets/${biobank}.csv" \
-            --target_build "$target_build" \
+            --target_build "${target_build}" \
             --scorefile "$scorefiles" \
             --outdir "$output_dir" \
             --verbose \
             --min_overlap "$min_overlap" \
-            --run_ancestry "$(pwd)/pgsc_calc_requirements/reference_data/pgsc_HGDP+1kGP_v1.tar.zst" \
-            "${extra_params[@]}"
+            --run_ancestry "$(pwd)/pgsc_calc_requirements/reference_data/pgsc_HGDP+1kGP_v1.tar.zst"
 
 echo "Job finished with exit code $? at: `date`"
